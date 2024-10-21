76bde08f2b1353b073ca5060f2b7be3287ff2253
Evvan Erb <evvan.erb@vertexinc.com>
2024-10-21 18:16:04 -0400

    Add SQL insert gen, add array support, refactor for clarity and simplicity, add more path arg support

---
+++ src/example_outputs/exampleObjectsInsert.sql
@@ -0,0 +1,16 @@
+INSERT INTO userUUIDuserNameissuerclaimNameclaimValue (userUUID, userName, issuer, claimName, claimValue) VALUES (321ab8a2-0405-4cda-9fdc-7a3ef614e671, 91pRAUIIZw, l49iGoBUCJ, xVW1alVtzI, ZQZOeUeBov);
+INSERT INTO userUUIDuserNameissuerclaimNameclaimValue (userUUID, userName, issuer, claimName, claimValue) VALUES (34ebfc22-2759-4c4c-bb88-fefab1fa8227, W1CpTnCs5t, EjpKdWR24P, hsgt10LvyT, T3DvBcaBwN);
+INSERT INTO userUUIDuserNameissuerclaimNameclaimValue (userUUID, userName, issuer, claimName, claimValue) VALUES (6dc311e0-5ea8-46c5-b67a-3168d80d32ad, TinS4xPPpq, 3quLUx1H9x, TEOTwZxb1Y, KWKEJ47HRN);
+
+INSERT INTO subValueOnesubValueTwo (subValueOne, subValueTwo) VALUES (243TRqr5vR, 17);
+INSERT INTO subValueOnesubValueTwo (subValueOne, subValueTwo) VALUES (WxUSUA2Qhb, 22);
+INSERT INTO subValueOnesubValueTwo (subValueOne, subValueTwo) VALUES (LhzXGbKGWT, 60);
+
+INSERT INTO valueOnevalueTwovalueThreevalueFourvalueFive (valueOne, valueTwo, valueThree, valueFour, valueFive) VALUES (SIrCHqA6Cy, -68.24, 57, true, [object Object]);
+INSERT INTO valueOnevalueTwovalueThreevalueFourvalueFive (valueOne, valueTwo, valueThree, valueFour, valueFive) VALUES (LPognyZPmS, -62.11, 27, true, [object Object]);
+INSERT INTO valueOnevalueTwovalueThreevalueFourvalueFive (valueOne, valueTwo, valueThree, valueFour, valueFive) VALUES (bV1HkYZKPK, -17.08, 17, true, [object Object]);
+
+INSERT INTO idmyObject (id, myObject) VALUES (100, [object Object]);
+INSERT INTO idmyObject (id, myObject) VALUES (21, [object Object]);
+INSERT INTO idmyObject (id, myObject) VALUES (67, [object Object]);
+
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ const fs   = require('fs');
 
 let savedRefs = {};
 let exampleObjects = {};
+let oasDirectory = "";
+let inputOasFilePath = "";
 
 function generateRandomString(length) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -24,6 +26,29 @@ function generateUUID() {
   });
 }
 
+function generateInsertExampleSql() {
+  // TODO deal with sub objects
+  let sql = "";
+  // Iterate over all types of example objects
+  for (const objectName of Object.keys(exampleObjects)) {
+    // Iterate over all 3 generated examples of that type
+    for (const exampleObject of exampleObjects[objectName]) {
+      sql += "INSERT INTO " + objectName + " (";
+      // Iterate over all properties for that object
+      for (const property of Object.keys(exampleObject)) {
+        sql += property + ", ";
+      }
+      sql = sql.slice(0, -2) + ") VALUES (";
+      for (const property of Object.keys(exampleObject)) {
+        sql += exampleObject[property] + ", ";
+      }
+      sql = sql.slice(0, -2) + ");\n";
+    }
+    sql += "\n";
+  }
+  return sql;
+}
+
 function generateBody(schema) {
   if (schema['type'] == 'array') {
     // If it is an array of objects get all 3 example objects
@@ -35,12 +60,39 @@ function generateBody(schema) {
         exampleObjects[objectName][2],
       ]
     }
-    // TODO create a set of 3 for other types
+    // For other types
+    return generateExampleArray(schema['items']['type']);
   } else if (schema['type'] == 'object') {
     // If it is an object return first example object
     return exampleObjects[Object.keys(schema['properties']).join('')][0];
   }
-  // TODO return examples for non arrays or objects
+  // Return singular example for non arrays or objects
+  return generateExampleItem(schema['type']);
+}
+
+function generateExampleItem(type) {
+  if (type == 'string') {
+    return generateRandomString(10);
+  } else if (type == 'uuid') {
+    return generateUUID();
+  } else if (type == 'integer') {
+   return (Math.floor(Math.random() * (100 - 1 + 1)) + 1);
+  } else if (type== 'number') {
+    return (Math.round((Math.random() * (1 - 100) + 1) * 100) / 100);
+  } else if (type == 'boolean') {
+    return true;
+  } else {
+    return null;
+  }
+}
+
+function generateExampleArray(type) {
+  // TODO handle nested arrays
+  let returnArray = [];
+  returnArray.push(generateExampleItem(type));
+  returnArray.push(generateExampleItem(type));
+  returnArray.push(generateExampleItem(type));
+  return returnArray;
 }
 
 function generateExampleObjects(objectName, schema) {
@@ -56,36 +108,28 @@ function generateExampleObjects(objectName, schema) {
   // Generate example data for each property by type
   for (propertyName in schema['properties']) {
     let currentProperty = schema['properties'][propertyName];
+    // Get the type
+    let type = currentProperty['type'];
     if (currentProperty['type'] == 'string') {
       if (currentProperty['format'] != null && currentProperty['format'] == 'uuid') {
-        exampleOne[propertyName] = generateUUID();
-        exampleTwo[propertyName] = generateUUID();
-        exampleThree[propertyName] = generateUUID();
-      } else {
-        exampleOne[propertyName] = generateRandomString(10);
-        exampleTwo[propertyName] = generateRandomString(10);
-        exampleThree[propertyName] = generateRandomString(10);
+        type = 'uuid';
       }
-    } else if (currentProperty['type'] == 'integer') {
-      exampleOne[propertyName] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-      exampleTwo[propertyName] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-      exampleThree[propertyName] = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-    } else if (currentProperty['type'] == 'number') {
-      exampleOne[propertyName] = Math.round((Math.random() * (1 - 100) + 1) * 100) / 100;
-      exampleTwo[propertyName] = Math.round((Math.random() * (1 - 100) + 1) * 100) / 100;
-      exampleThree[propertyName] = Math.round((Math.random() * (1 - 100) + 1) * 100) / 100;
-    } else if (currentProperty['type'] == 'boolean') {
-      exampleOne[propertyName] = true;
-      exampleTwo[propertyName] = true;
-      exampleThree[propertyName] = true;
-    } else if (currentProperty['type'] == 'array') {
-      // TODO
-    } else if (currentProperty['type'] == 'object') {
+    }
+
+    if (type == 'array') {
+      exampleOne[propertyName] = generateExampleArray(currentProperty['items']['type']);
+      exampleTwo[propertyName] = generateExampleArray(currentProperty['items']['type']);
+      exampleThree[propertyName] = generateExampleArray(currentProperty['items']['type']);
+    } else if (type == 'object') {
       // Due to the recursion strucutre in resolveReferences, sub-objects should already have examples generated
       const subObjectName = Object.keys(currentProperty['properties']).join('');
       exampleOne[propertyName] = exampleObjects[subObjectName][0];
       exampleTwo[propertyName] = exampleObjects[subObjectName][1];
       exampleThree[propertyName] = exampleObjects[subObjectName][2];
+    } else {
+      exampleOne[propertyName] = generateExampleItem(type);
+      exampleTwo[propertyName] = generateExampleItem(type);
+      exampleThree[propertyName] = generateExampleItem(type);
     }
   }
 
@@ -105,7 +149,8 @@ function fetchReferences(ref) {
   if (ref.includes('.yaml') || ref.includes('.yml')) {
     const path = ref.split('#')[0];
     const componentName = ref.split('/').at(-1);
-    const schema = yaml.load(fs.readFileSync(path, 'utf8')).components.schemas[componentName];
+    const relativePath = oasDirectory + '/' + path.substring(path.lastIndexOf('/') + 1);
+    const schema = yaml.load(fs.readFileSync(relativePath, 'utf8')).components.schemas[componentName];
     let resolvedSchema = resolveReferences(schema);
     resolvedSchema['name'] = ref;
     savedRefs[ref] = resolvedSchema;
@@ -113,7 +158,7 @@ function fetchReferences(ref) {
   }
 
   // Otherwise load from local file
-  const openApiSpec = yaml.load(fs.readFileSync('./exampleOAS.yaml', 'utf8'));
+  const openApiSpec = yaml.load(fs.readFileSync(inputOasFilePath, 'utf8'));
   const componentName = ref.split('/').at(-1);
   let resolvedSchema = resolveReferences(openApiSpec.components.schemas[componentName]);
   resolvedSchema['name'] = '#/components/schemas/' + componentName;
@@ -149,9 +194,55 @@ function resolveReferences(schema) {
   return schema;
 }
 
+function generatePactInteraction(openApiSpec, path, requestMethod) {
+  // Init the current pact interaction
+  let pactInteraction = {};
+  pactInteraction['description'] = path + " " + requestMethod;
+  pactInteraction['providerStates'] = [];
+  let providerState = {};
+  providerState['name'] = path + " " + requestMethod;
+  pactInteraction['providerStates'].push(providerState);
+  // Add request info
+  pactInteraction['request'] = {};
+  pactInteraction['request']['method'] = requestMethod.toUpperCase();
+  pactInteraction['request']['path'] = path;
+
+  // Get successful request and response bodies for each request method
+  let requestBody = null;
+  let responseBody = null;
+  if (openApiSpec.paths[path][requestMethod].requestBody != null) {
+    requestBody = openApiSpec.paths[path][requestMethod].requestBody.content['application/json'].schema;
+  }
+  if (openApiSpec.paths[path][requestMethod].responses['200'] != null && openApiSpec.paths[path][requestMethod].responses['200'].content != null) {
+    responseBody = openApiSpec.paths[path][requestMethod].responses['200'].content['application/json'].schema;
+  }
+  const parsedRequestBodySchema = resolveReferences(requestBody);
+  const parsedResponseBodySchema = resolveReferences(responseBody);
+
+  // Add Request Body
+  if (parsedRequestBodySchema != null) {
+    pactInteraction['request']['headers'] = {};
+    pactInteraction['request']['headers']['Content-Type'] = 'application/json';
+    pactInteraction['request']['body'] = generateBody(parsedRequestBodySchema);
+  }
+
+  // Add Response Body
+  pactInteraction['response'] = {};
+  pactInteraction['response']['status'] = 200;
+  if (parsedResponseBodySchema != null) {
+    pactInteraction['response']['headers'] = {};
+    pactInteraction['response']['headers']['Content-Type'] = 'application/json';
+    pactInteraction['response']['body'] = generateBody(parsedResponseBodySchema);
+  }
+
+  return pactInteraction;
+}
+
 function main() {
   // Load openapi spec yaml
-  const openApiSpec = yaml.load(fs.readFileSync(process.argv.slice(2)[0], 'utf8'));
+  inputOasFilePath = process.argv.slice(2)[0];
+  const openApiSpec = yaml.load(fs.readFileSync(inputOasFilePath, 'utf8'));
+  oasDirectory = inputOasFilePath.substring(0, inputOasFilePath.lastIndexOf('/'));
 
   // Init pact contract
   let pactContract = {};
@@ -169,52 +260,18 @@ function main() {
 
     // Get all request methods for each path
     for (requestMethod in openApiSpec.paths[path]) {
-      // Init the current pact interaction
-      let pactInteraction = {};
-      pactInteraction['description'] = path + " " + requestMethod;
-      pactInteraction['providerStates'] = [];
-      let providerState = {};
-      providerState['name'] = path + " " + requestMethod;
-      pactInteraction['providerStates'].push(providerState);
-      // Add request info
-      pactInteraction['request'] = {};
-      pactInteraction['request']['method'] = requestMethod.toUpperCase();
-      pactInteraction['request']['path'] = path;
-
-      // Get successful request and response bodies for each request method
-      let requestBody = null;
-      let responseBody = null;
-      if (openApiSpec.paths[path][requestMethod].requestBody != null) {
-        requestBody = openApiSpec.paths[path][requestMethod].requestBody.content['application/json'].schema;
-      }
-      if (openApiSpec.paths[path][requestMethod].responses['200'] != null && openApiSpec.paths[path][requestMethod].responses['200'].content != null) {
-        responseBody = openApiSpec.paths[path][requestMethod].responses['200'].content['application/json'].schema;
-      }
-      const parsedRequestBodySchema = resolveReferences(requestBody);
-      const parsedResponseBodySchema = resolveReferences(responseBody);
-
-      // Add Request Body
-      if (parsedRequestBodySchema != null) {
-        pactInteraction['request']['headers'] = {};
-        pactInteraction['request']['headers']['Content-Type'] = 'application/json';
-        pactInteraction['request']['body'] = generateBody(parsedRequestBodySchema);
-      }
-
-      // Add Response Body
-      pactInteraction['response'] = {};
-      pactInteraction['response']['status'] = 200;
-      if (parsedResponseBodySchema != null) {
-        pactInteraction['response']['headers'] = {};
-        pactInteraction['response']['headers']['Content-Type'] = 'application/json';
-        pactInteraction['response']['body'] = generateBody(parsedResponseBodySchema);
-      }
-
-      // Add interaction
+      // Add Interaction
+      const pactInteraction = generatePactInteraction(openApiSpec, path, requestMethod);
       pactContract['interactions'].push(pactInteraction);
     }
   }
 
-  fs.writeFileSync('examplePactContract.json', JSON.stringify(pactContract, null, 2), 'utf8');
+  // Output pact contract
+  fs.writeFileSync('./example_outputs/examplePactContract.json', JSON.stringify(pactContract, null, 2), 'utf8');
+  // Output example objects
+  fs.writeFileSync('./example_outputs/exampleObjects.json', JSON.stringify(exampleObjects, null, 2), 'utf8');
+  // Output example objects as insert SQL
+  fs.writeFileSync('./example_outputs/exampleObjectsInsert.sql', generateInsertExampleSql(), 'utf8');
 }
 
 main();
